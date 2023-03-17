dd2797ca8c84c10c3cd437ca3284ab725b88802c
Nicolas Jegat <njegat@2B7.42angouleme.fr>
2023-03-17 16:55:35 +0100

    modification buffer pwd (PATH_MAX)

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -3,19 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ltuffery <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: njegat <njegat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 15:20:55 by ltuffery          #+#    #+#             */
-/*   Updated: 2023/03/17 15:22:54 by ltuffery         ###   ########.fr       */
+/*   Updated: 2023/03/17 16:52:36 by njegat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
+#include <limits.h>
 
 void	pwd_builtins(void)
 {
-	char	cwd[256];
+	char	cwd[PATH_MAX];
 
 	if (getcwd(cwd, sizeof(cwd)) == NULL)
 		perror("error cwd");
