50a43564b7b1328b36ca5d1f3362129244a034f8
Nicolas Jegat <njegat@2A7.42angouleme.fr>
2023-03-21 17:43:57 +0100

    Fix and split fonction
    fonction get_cmd qui decoupe la commande

--- src/lexer/split_prompt.c
+++ src/lexer/split_prompt.c
@@ -6,57 +6,78 @@
 /*   By: njegat <njegat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 12:44:17 by njegat            #+#    #+#             */
-/*   Updated: 2023/03/21 15:06:37 by njegat           ###   ########.fr       */
+/*   Updated: 2023/03/21 17:00:18 by njegat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/lexer.h"
 
+static int	skip_redirect(char *cmd, int pos)
+{
+	while (cmd[pos] == '>' || cmd[pos] == '<')
+		pos++;
+	while (cmd[pos] == ' ')
+		pos++;
+	while (cmd[pos] != ' ' && cmd[pos])
+		pos++;
+	// while (cmd[pos] == ' ')
+	// 	pos++;
+	return (pos);
+}
+
+static int	is_quote(char *cmd, int pos)
+{
+	static int	s_quote;
+	static int	d_quote;
+
+	if (pos == -1)
+		return ((s_quote + d_quote) % 2);
+	if (cmd[pos] == '"' && (s_quote) == 0)
+	{
+		if (d_quote)
+			d_quote = 0;
+		else
+			d_quote = 1;
+		return (1);
+	}
+	else if (cmd[pos] == '\'' && (d_quote) == 0)
+	{
+		if (s_quote)
+			s_quote = 0;
+		else
+			s_quote = 1;
+		if (cmd[pos + 1] == '$')
+			return (0);
+		return (1);
+	}
+	return (0);
+}
+
 void	get_cmd(t_data *data, char *cmd)
 {
-	size_t	i;
-	int	j;
-	int	s_quote;
-	int	d_quote;
+	int		i;
+	int		j;
 	char	*tmp;
 
 	i = 0;
 	j = 0;
-	s_quote = 0;
-	d_quote = 0;
 	tmp = malloc(ft_strlen(cmd) * sizeof(char));
 	while (cmd[i] == ' ')
 		i++;
 	while (cmd[i])
 	{
-		if (cmd[i] == '"' && (s_quote % 2) == 0)
-		{
-			d_quote++;
+		if (is_quote(cmd, i))
 			i++;
-			continue ;
-		}
-		if (cmd[i] == '\'' && (d_quote % 2) == 0)
-		{
-			s_quote++;
-			i++;
-			continue ;
-		}
-		if ((cmd[i] == '>' || cmd[i] == '<')  && !(s_quote % 2) && !(d_quote % 2))
-		{
-			while (cmd[i] == '>' || cmd[i] == '<')
-				i++;
-			while (cmd[i] == ' ')
-				i++;
-			while (cmd[i] != ' ' && cmd[i])
-				i++;
-			while (cmd[i] == ' ')
-				i++;
-			continue ;
-		}
-		if (cmd[i] == ' ' && !(s_quote % 2) && !(d_quote % 2))
+		else if ((cmd[i] == '>' || cmd[i] == '<')  && !is_quote(NULL, -1))
+			i = skip_redirect(cmd, i);
+		else if (cmd[i] == ' ' && !is_quote(NULL, -1))
 		{
 			tmp[j] = 0;
-			data->cmdx = ft_strappend(tmp, data->cmdx);
+			j = 0;
+			while (tmp[j] == ' ')
+				j++;
+			if (tmp[j])
+				data->cmdx = ft_strappend(tmp, data->cmdx);
 			j = 0;
 			while (cmd[i] == ' ')
 				i++;
@@ -67,13 +88,9 @@ void	get_cmd(t_data *data, char *cmd)
 			j++;
 			i++;
 		}
-		
 	}
 	tmp[j] = 0;
-	j = 0;
-	while (tmp[j] == ' ')
-		j++;
-	if (tmp[j])
+	if (tmp[0])
 		data->cmdx = ft_strappend(tmp, data->cmdx);
 	free(tmp);
 }
