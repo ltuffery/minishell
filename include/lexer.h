046fa9f5e3548b0cf5aa222d044901d3d4cbf021
ltuffery <ltuffery@student.42.fr>
2023-03-21 17:51:49 +0100

    doc update

--- include/lexer.h
+++ include/lexer.h
@@ -6,7 +6,7 @@
 /*   By: njegat <njegat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 18:54:11 by njegat            #+#    #+#             */
-/*   Updated: 2023/03/20 16:53:47 by njegat           ###   ########.fr       */
+/*   Updated: 2023/03/21 17:51:30 by ltuffery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,17 @@
 # include <stdlib.h>
 # include "../libft/libft.h"
 
+
+//	INFILE = "<"
+//	HERE_DOC = "<<"
+//	OUTFILE = ">"
+//	APPEND = ">>"
 typedef enum e_type_file
 {
-	INFILE = 1, //<
-	HERE_DOC = 2, //<<
-	OUTFILE = 3, //>
-	APPEND = 4, //>>
+	INFILE = 1,
+	HERE_DOC = 2,
+	OUTFILE = 3,
+	APPEND = 4,
 }	t_type_file;
 
 typedef struct s_file
