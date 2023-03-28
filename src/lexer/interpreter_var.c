2593c728109c65d6c9877eec7129c5763ff84ea3
ltuffery <ltuffery@student.42.fr>
2023-03-28 18:15:56 +0200

    Update du var_value
    Meilleurs update du var_value

--- src/lexer/interpreter_var.c
+++ src/lexer/interpreter_var.c
@@ -6,26 +6,36 @@
 /*   By: ltuffery <ltuffery@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 12:22:38 by ltuffery          #+#    #+#             */
-/*   Updated: 2023/03/27 13:10:52 by ltuffery         ###   ########.fr       */
+/*   Updated: 2023/03/28 18:03:33 by ltuffery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../libft/libft.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 char	*var_value(char *line)
 {
-	char	*var;
+	size_t	i;
 	char	*ret;
 	char	*value;
 
-	var = ft_strtrim(line, "\"' ");
-	ret = ft_strchr(var, '$');
+	i = 0;
+	if (line[i] == '$')
+		i++;
+	ret = ft_strdup(&line[i]);
+	i = 0;
 	if (ret == NULL)
 		return (NULL);
-	value = getenv(&ret[1]);
-	free(var);
+	while (ft_isalnum(ret[i]) || ret[i] == '_')
+		i++;
+	ret[i] = '\0';
+	if (ft_isdigit(ret[0]))
+		value = ft_strdup(&ret[1]);
+	else
+		value = getenv(ret);
+	free(ret);
 	if (value == NULL)
-		return (line);
+		return (NULL);
 	return (value);
 }
