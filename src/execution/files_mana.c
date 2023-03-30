dd8882e9bac5b2f7de7c51b87a114903281ab4a4
ltuffery <ltuffery@student.42.fr>
2023-03-30 18:03:16 +0200

    Opti file mana
    Suppression du code repetitif

--- src/execution/files_mana.c
+++ src/execution/files_mana.c
@@ -6,92 +6,77 @@
 /*   By: njegat <njegat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 15:35:33 by njegat            #+#    #+#             */
-/*   Updated: 2023/03/29 16:58:36 by njegat           ###   ########.fr       */
+/*   Updated: 2023/03/30 17:59:34 by ltuffery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/execute.h"
 
-int	open_infile(t_cmd *cmd)
+int	open_infile(t_file *file, int *fd_infile)
 {
-	t_file	*tmp;
-
-	tmp = cmd->file;
-	while (tmp)
+	if (file->type == INFILE)
 	{
-		if (tmp->type == INFILE)
+		file->fd = open(file->name, O_RDONLY, 0644);
+		*fd_infile = file->fd;
+		if (file->fd == -1)
 		{
-			tmp->fd = open(tmp->name, O_RDONLY, 0644);
-			cmd->fd_infile = tmp->fd;
-			if (tmp->fd == -1)
-			{
-				ft_print_error_file(tmp->name);
-				return (1);
-			}
+			ft_print_error_file(file->name);
+			return (1);
 		}
-		tmp = tmp->next;
 	}
 	return (0);
 }
 
-int	open_heredoc(t_cmd *cmd)
+int	open_heredoc(t_file *file, int *fd_infile)
 {
-	t_file	*tmp;
-
-	tmp = cmd->file;
-	while (tmp)
+	if (file->type == HERE_DOC)
 	{
-		if (tmp->type == HERE_DOC)
+		//tmp->fd = get_here_doc();
+		*fd_infile = file->fd;
+		if (file->fd == -1)
 		{
-			//tmp->fd = get_here_doc();
-			cmd->fd_infile = tmp->fd;
-			if (tmp->fd == -1)
-			{
-				perror("minishoull: here_doc: ");
-				return (1);
-			}
+			perror("minishoull: here_doc: ");
+			return (1);
 		}
-		tmp = tmp->next;
 	}
 	return (0);
 }
 
-int	open_out(t_cmd *cmd)
+int	open_out(t_file *file, int *fd_outfile)
 {
-	t_file	*tmp;
-
-	tmp = cmd->file;
-	while (tmp)
+	if (file->type == OUTFILE || file->type == APPEND)
 	{
-		if (tmp->type == OUTFILE || tmp->type == APPEND)
+		if (file->type == OUTFILE)
+			file->fd = open(file->name, O_TRUNC | O_CREAT | O_WRONLY, 0644);
+		else if (file->type == APPEND)
+			file->fd = open(file->name, O_APPEND | O_CREAT | O_WRONLY, 0644);
+		*fd_outfile = file->fd;
+		if (file->fd == -1)
 		{
-			if (tmp->type == OUTFILE)
-				tmp->fd = open(tmp->name, O_TRUNC | O_CREAT | O_WRONLY, 0644);
-			else if (tmp->type == APPEND)
-				tmp->fd = open(tmp->name, O_APPEND | O_CREAT | O_WRONLY, 0644);
-			cmd->fd_outfile = tmp->fd;
-			if (tmp->fd == -1)
-			{
-				ft_print_error_file(tmp->name);
-				return (1);
-			}
+			ft_print_error_file(file->name);
+			return (1);
 		}
-		tmp = tmp->next;
 	}
 	return (0);
 }
 
 int	open_files(t_cmd *cmd)
 {
-	int	infile_check;
-	int	heredoc_check;
-	int	out_check;
+	int		infile_check;
+	int		heredoc_check;
+	int		out_check;
+	t_file	*file;
 
-	heredoc_check = open_heredoc(cmd);
-	infile_check = open_infile(cmd);
-	out_check = open_out(cmd);
-	if (heredoc_check || infile_check || out_check)
-		return (1);
+	file = cmd->file;
+	while (file != NULL)
+	{
+		heredoc_check = open_heredoc(file, &cmd->fd_infile);
+		infile_check = open_infile(file, &cmd->fd_infile);
+		out_check = open_out(file, &cmd->fd_outfile);
+		if (heredoc_check || infile_check || out_check)
+			return (1);
+		file = file->next;
+	}
 	return (0);
 }
 
