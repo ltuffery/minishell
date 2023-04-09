7ab1d9a7cb46877b5c31bbff823ae4f526007c49
Nicolas Jegat <njegat@2D1.42angouleme.fr>
2023-04-09 14:55:48 +0200

    amelioration tester

--- test_cmd.sh
+++ test_cmd.sh
@@ -24,7 +24,32 @@ if [ "$1" -eq "1" ]
 					./minishell <ret 2>minsh 1>tmp;
 					bash <ret 2>bsh 1>tmp;
 					cat minsh | sed "/┌─/d" | sed "/└─/d"  >ret
-					if [ -s ret ] && [ -s bsh ]
+					err=0
+					for nom in "no such file or directory\|No such file or directory" "command not found\|Command not found" "syntax error near unexpected token '>\|syntax error near unexpected token \`>" "syntax error near unexpected token '<\|syntax error near unexpected token \`<" "syntax error near unexpected token '|\|syntax error near unexpected token \`|"
+					do
+  						err1=`cat bsh | grep -a "$nom" | wc -l`
+						err2=`cat ret | grep -a "$nom" | wc -l`
+						#echo "err1 = $err1,  err2 = $err2,  nom = $nom\n"
+						if ([ -s ret ] && [ -s bsh ]) && [ "$err1" -ne "$err2" ]
+							then
+								err=1
+								echo "\033[0;31m"$ligne"\033[0m"
+								echo "------------"
+								cat <ret
+								cat <bsh
+								echo "----------\n\n"
+								break
+							else
+								if ([ -s ret ] && [ -s bsh ]) && [ "$err1" -eq "1" ] && [ "$err2" -eq "1" ]
+									then
+										err=1
+										break
+									else
+										err=0
+								fi
+						fi
+					done
+					if ([ -s ret ] && [ -s bsh ]) && [ "$err" -eq "0" ]
 						then
 							echo "\033[0;33m"$ligne"\033[0m"
 							echo "------------"
@@ -34,6 +59,16 @@ if [ "$1" -eq "1" ]
 						else
 							echo -n
 					fi
+					# if [ -s ret ] && [ -s bsh ] && [ "$err1" -ne "$err2" ]
+					# 	then
+					# 		echo "\033[0;33m"$ligne"\033[0m"
+					# 		echo "------------"
+					# 		cat <ret
+					# 		cat <bsh
+					# 		echo "----------\n\n"
+					# 	else
+					# 		echo -n
+					# fi
 				done;
 			else
 			echo "1 for back cmd and 2 for error"
