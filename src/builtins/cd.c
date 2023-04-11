7cec8805b3643551a8bc52163e6591ac7ed429cc
Nicolas Jegat <njegat@2D5.42angouleme.fr>
2023-04-11 15:41:15 +0200

    modif cd
    modification de cd pour géré les var d'env inexisante.
    creation d'une fonction pour print les erreur de cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: njegat <njegat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 15:26:09 by ltuffery          #+#    #+#             */
-/*   Updated: 2023/04/11 14:51:18 by njegat           ###   ########.fr       */
+/*   Updated: 2023/04/11 15:38:17 by njegat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 #include <limits.h>
 #include <stdio.h>
 #include <unistd.h>
+#define MANY_ARG 1
+#define NSET 2
+#define ERRDIR 3
 
 void	pwd_change(t_data *data)
 {
@@ -44,39 +47,58 @@ void	pwd_change(t_data *data)
 	ft_double_free(export);
 }
 
+static void	puterror_cd(char *arg, int err)
+{
+	if (err == MANY_ARG)
+		ft_putstr_fd("minishoul: cd: too many arguments\n", 2);
+	else if (err == NSET)
+	{
+		ft_putstr_fd("minishoul: cd: ", 2);
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(" not set\n", 2);
+	}
+	else if (err == ERRDIR)
+	{
+		ft_putstr_fd("minishoul: cd: ", 2);
+		perror(arg);
+	}
+}
+
 void	cd_builtins(t_data *data, char **arg)
 {
 	int		error;
 	char	*tmp;
 
+	error = 0;
 	if (arg[2])
-	{
-		ft_putstr_fd("minishoul: cd: ", 2);
-		ft_putstr_fd("too many arguments\n", 2);
-		return ;
-	}
+		puterror_cd(NULL, MANY_ARG);
 	else if (!arg[1] || !ft_strcmp(arg[1], "~"))
 	{
 		tmp = getvalue(data->env, "HOME");
-		error = chdir(tmp);
-		free(tmp);
+		if (tmp)
+		{
+			error = chdir(tmp);
+			free(tmp);
+		}
+		else
+			puterror_cd("HOME", NSET);
 	}
 	else if (!ft_strcmp(arg[1], "-"))
 	{
 		tmp = getvalue(data->env, "OLDPWD");
-		error = chdir(tmp);
-		pwd_builtins();
-		free(tmp);
+		if (tmp)
+		{
+			error = chdir(tmp);
+			pwd_builtins();
+			free(tmp);
+		}
+		else
+			puterror_cd("OLDPWD", NSET);
 	}
 	else
 		error = chdir(arg[1]);
 	if (error < 0)
-	{
-		ft_putstr_fd("minishoul: cd: ", 2);
-		perror(arg[1]);
-	}
+		puterror_cd(arg[1], ERRDIR);
 	else
-	{
 		pwd_change(data);
-	}
 }
