b4afe9241c345853ece7b127b4c28562de3cfbca
ltuffery <ltuffery@student.42.fr>
2023-04-18 14:27:14 +0200

    remove doublon de code

--- src/lexer/get_cmd.c
+++ src/lexer/get_cmd.c
@@ -6,7 +6,7 @@
 /*   By: njegat <njegat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 12:59:47 by njegat            #+#    #+#             */
-/*   Updated: 2023/04/18 12:56:09 by njegat           ###   ########.fr       */
+/*   Updated: 2023/04/18 14:23:01 by ltuffery         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,17 +73,14 @@ static int	insert_var(char *cmd, char *add, char **tmp)
 	int	i;
 
 	i = var_len(cmd);
+	if (!add)
+		return (i);
 	if (!*tmp)
 	{
 		*tmp = malloc(1);
 		*tmp[0] = 0;
 	}
-	if ((cmd[1] == ' ' || cmd[1] == '\t') && *tmp)
-		ft_strjoin(*tmp, "$");
-	else if (!add)
-		return (i);
-	else
-		*tmp = ft_strjoin(*tmp, add);
+	*tmp = ft_strjoin(*tmp, add);
 	free(add);
 	return (i);
 }
