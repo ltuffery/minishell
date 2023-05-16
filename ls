02c1b93fec4ced994656a7373e929f5d2bce9d0d
Nicolas Jegat <njegat@1H1.42angouleme.fr>
2023-05-16 07:19:23 +0200

    modif du recherche de PATH (exec)
    ne recherche pas le path seulement si il touvre un '/' et qu'il y a acces.
    si ce n'est pas le cas il cherche dans les path.
    si il ne trouve pas il exectute la commande tel quel

---
+++ ls
@@ -0,0 +1,5 @@
+#/bin/bash
+
+echo coucou;
+ls;
+exit
--- src/execution/get_path.c
+++ src/execution/get_path.c
@@ -6,7 +6,7 @@
 /*   By: njegat <njegat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 18:28:44 by njegat            #+#    #+#             */
-/*   Updated: 2023/05/09 07:06:47 by njegat           ###   ########.fr       */
+/*   Updated: 2023/05/16 07:14:52 by njegat           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,65 @@ static char	**get_path(char **env)
 	return (output);
 }
 
+// int	check_absolute_path(t_cmd *cmd)
+// {
+// 	char	*tmp;
+// 	int		jump;
+// 	int		error;
+
+// 	error = 0;
+// 	if (ft_strfind(cmd->arg[0], '/'))
+// 	{
+// 		if (access(cmd->arg[0], X_OK) == 0)
+// 		{
+// 			tmp = ft_strdup(cmd->arg[0]);
+// 			jump = 0;
+// 			while (tmp)
+// 			{
+// 				if (tmp[0] != '.' || tmp[1] != '/')
+// 					break ;
+// 				jump += 2;
+// 				free(tmp);
+// 				tmp = ft_strdup(cmd->arg[0] + jump);
+// 			}
+// 			if (!tmp)
+// 				error = 1;
+// 			else if (!tmp[0])
+// 				error = 1;
+// 			else
+// 			{
+// 				free(cmd->arg[0]);
+// 				cmd->arg[0] = ft_strdup(tmp);
+// 			}
+// 			if (!cmd->arg[0])
+// 				error = 1;
+// 		}
+// 	}
+// 	return (error);
+// }
+
+// int	check_absolute_path(t_cmd *cmd)
+// {
+// 	char	*tmp;
+// 	int		jump;
+// 	int		error;
+
+// 	error = 0;
+// 	if (ft_strfind(cmd->arg[0], '/'))
+// 	{
+// 		if (access(cmd->arg[0], X_OK) == 0)
+// 		{
+// 			jump = skip_set(cmd->arg[0], "./");
+// 			tmp = strdup(cmd->arg[0]);
+// 			if (!tmp)
+// 				return (1);
+// 			free(cmd->arg[0]);
+// 			cmd->arg[0] = tmp;
+// 		}
+// 	}
+// 	return (error);
+// }
+
 int	get_cmd_path(t_data *data, t_cmd *cmd)
 {
 	char	**path;
@@ -33,7 +92,7 @@ int	get_cmd_path(t_data *data, t_cmd *cmd)
 
 	if (!cmd->arg[0][0])
 		return (0);
-	if (access(cmd->arg[0], X_OK) == 0)
+	if (ft_strfind(cmd->arg[0], '/') && access(cmd->arg[0], X_OK) == 0)
 		return (0);
 	path = get_path(data->env);
 	if (!path)
